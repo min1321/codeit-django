915143d036905d261543f23ca127343dd67a7392
sukmin <mellonggo@naver.com>
2021-06-10 20:58:48 +0900

    Make Model

--- foods/models.py
+++ foods/models.py
@@ -1,5 +1,16 @@
 # 데이터 베이스와 연동을 관리하는 파일
 
 from django.db import models
+from django.db.models.fields import IntegerField
 
 # Create your models here.
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    price = models.IntegerField()
+    img_path = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
