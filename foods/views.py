d22d9cf10644796f844c952ed0a8a0d4c39222f6
sukmin <mellonggo@naver.com>
2021-06-10 20:51:32 +0900

    make error

--- foods/views.py
+++ foods/views.py
@@ -4,6 +4,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.http import Http404
 
 # Create your views here.
 
@@ -21,4 +22,6 @@ def food_detail(request, food):
         context['description'] = '주머니가 가벼운 당신의 마음까지 생각한 가격 !'
         context['price'] = 10000
         context['img_path'] = 'foods/images/chicken.jpg'
+    else:
+        raise Http404("이런 음식은 없다구요 !")
     return render(request, 'foods/detail.html', context=context)
