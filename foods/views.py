9a3e6eb0eaf31a596c94d488da1c0bd017b181da
sukmin <mellonggo@naver.com>
2021-06-30 17:20:19 +0900

    17 project complite

--- foods/views.py
+++ foods/views.py
@@ -19,13 +19,8 @@ def index(request):
     return render(request, 'foods/index.html', context=context)
 
 
-def food_detail(request, food):
+def food_detail(request, pk):
     context = dict()
-    if food == 'chicken':
-        context['name'] = '코딩에 빠진 닭'
-        context['description'] = '주머니가 가벼운 당신의 마음까지 생각한 가격 !'
-        context['price'] = 10000
-        context['img_path'] = 'foods/images/chicken.jpg'
-    else:
-        raise Http404("이런 음식은 없다구요 !")
+    menu = Menu.objects.get(id=pk)
+    context['menu'] = menu
     return render(request, 'foods/detail.html', context=context)
