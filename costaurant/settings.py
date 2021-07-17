6b0ecccf52f5a8c7d79194a6500d64a9973f1cc6
sukmin <mellonggo@naver.com>
2021-07-17 15:55:41 +0900

    collectstatic

--- costaurant/settings.py
+++ costaurant/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3ax-iq$z&v=in*@7j6ex@h*6rx-zo@-5_m=#thr24$63#vz%i3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
